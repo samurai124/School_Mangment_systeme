651377aacd15aa450e26bf736b59fa8333a38815
Hamza <hamzazaidi253@gamil.com>
2025-12-29 16:47:59 +0100

    all

--- code/js/index.js
+++ code/js/index.js
@@ -17,45 +17,60 @@ document.addEventListener("click", (e) => {
     }
 });
 
-// students list :
-// const students = [
-//     {id : 1, name : "Zaidi Hamza" , email : "email@example.com" , group : 2},
-//     {id : 2, name : "Zaidi Hamza" , email : "email@example.com" , group : 2},
-//     {id : 3, name : "Zaidi Hamza" , email : "email@example.com" , group : 2},
-//     {id : 4, name : "Zaidi Hamza" , email : "email@example.com" , group : 2},
-//     {id : 5, name : "Zaidi Hamza" , email : "email@example.com" , group : 2},
-//     {id : 6, name : "Zaidi Hamza" , email : "email@example.com" , group : 2},
-//     {id : 7, name : "Zaidi Hamza" , email : "email@example.com" , group : 2},
-//     {id : 8, name : "Zaidi Hamza" , email : "email@example.com" , group : 2},
-//     {id : 9, name : "Zaidi Hamza" , email : "email@example.com" , group : 2},
-//     {id :10, name : "Zaidi Hamza" , email : "email@example.com" , group : 2},
-// ]
-// localStorage.setItem("students", JSON.stringify(students));
 
 
-// absent list
-const absents = [
-    { date: "2025-12-17", studentID: 1 },
-    { date: "2025-12-17", studentID: 2 },
-    { date: "2025-12-16", studentID: 3 },
-    { date: "2025-12-15", studentID: 3 },
-    { date: "2025-12-15", studentID: 3 },
-    { date: "2025-12-18", studentID: 3 },
-    { date: "2025-12-18", studentID: 3 },
-];
+function localStorageSettings() {
+    localStorage.clear();
+    // students list :
+    const initialStudents = [
+        { id: 1, name: "Zaidi Hamza", email: "email@example.com", group: 2, status: "active" },
+        { id: 2, name: "Zaidi Hamza", email: "email@example.com", group: 2, status: "active" },
+        { id: 3, name: "Zaidi Hamza", email: "email@example.com", group: 2, status: "active" },
+        { id: 4, name: "Zaidi Hamza", email: "email@example.com", group: 2, status: "active" },
+        { id: 5, name: "Zaidi Hamza", email: "email@example.com", group: 2, status: "active" },
+        { id: 6, name: "Zaidi Hamza", email: "email@example.com", group: 2, status: "active" },
+        { id: 7, name: "Zaidi Hamza", email: "email@example.com", group: 2, status: "active" },
+        { id: 8, name: "Zaidi Hamza", email: "email@example.com", group: 2, status: "active" },
+        { id: 9, name: "Zaidi Hamza", email: "email@example.com", group: 2, status: "active" },
+        { id: 10, name: "Zaidi Hamza", email: "email@example.com", group: 2, status: "active" },
+    ];
+
+    if (!localStorage.getItem("students")) {
+        localStorage.setItem("students", JSON.stringify(initialStudents));
+    }
+
+
+    // absent list
+    const absents = [
+        { date: "2025-12-26", studentID: 1 },
+        { date: "2025-12-27", studentID: 2 },
+        { date: "2025-12-28", studentID: 3 },
+        { date: "2025-12-29", studentID: 3 },
+        { date: "2025-12-29", studentID: 3 },
+        { date: "2025-12-29", studentID: 3 },
+        { date: "2025-12-29", studentID: 3 },
+    ];
+    if (!localStorage.getItem("absents")) {
+        localStorage.setItem("absents", JSON.stringify(absents));
+    }
+}
 
 
 
-localStorage.setItem("absents",JSON.stringify(absents));
+
+//localStorage.setItem("absents",JSON.stringify(absents));
 // retard list 
 const retards = [
-    { date: "2025-12-18", studentID: 3, minutesLate: 10 },
-    { date: "2025-12-18", studentID: 4, minutesLate: 10 },
-    { date: "2025-12-18", studentID: 5, minutesLate: 10 }
+    { date: "2025-12-26", studentID: 3, minutesLate: 10 },
+    { date: "2025-12-27", studentID: 4, minutesLate: 10 },
+    { date: "2025-12-28", studentID: 5, minutesLate: 10 }
 ];
+if (!localStorage.getItem("retards")) {
+    localStorage.setItem("retards", JSON.stringify(retards));
+}
 
 
-
+//localStorage.setItem("retards",JSON.stringify(retards));
 function getStudents() {
     let students = JSON.parse(localStorage.getItem("students")) || [];
     return students;
@@ -64,9 +79,9 @@ function getAbsents() {
     let absents = JSON.parse(localStorage.getItem("absents")) || [];
     return absents;
 }
-function getAbsentsBydates(date){
+function getAbsentsBydates(date) {
     let absents = JSON.parse(localStorage.getItem("absents")) || [];
-    absents = absents.filter((e)=> e.date == date) || [];
+    absents = absents.filter((e) => e.date == date) || [];
     return absents;
 }
 function getRetards() {
@@ -76,8 +91,8 @@ function getRetards() {
 
 function getRetardsBydate(date) {
     let retards = JSON.parse(localStorage.getItem("retards")) || [];
-    retards = retards.filter((e)=> e.date == date) || [];
-    return retards;    
+    retards = retards.filter((e) => e.date == date) || [];
+    return retards;
 }
 
 
@@ -103,9 +118,9 @@ const newRetard = {
     minutesLate: 10
 }
 function enregistrerRetard(newRetard) {
-    let absents = JSON.parse(localStorage.getItem("absents")) || [];
-    absents.push(newRetard);
-    localStorage.setItem("students", JSON.stringify(newRetard));
+    let retards = JSON.parse(localStorage.getItem("retards")) || [];
+    retards.push(newRetard);
+    localStorage.setItem("retards", JSON.stringify(retards));
 }
 
 
@@ -117,9 +132,9 @@ const newAbsent = {
     minutesLate: 10
 }
 function enregistrerAbsent(newAbsent) {
-    let retards = JSON.parse(localStorage.getItem("retards")) || [];
-    retards.push(newAbsent);
-    localStorage.setItem("students", JSON.stringify(newAbsent));
+    let absents = JSON.parse(localStorage.getItem("absents")) || [];
+    absents.push(newAbsent);
+    localStorage.setItem("absents", JSON.stringify(absents));
 }
 
 function getDayDate(daysAgo = 0) {
@@ -130,6 +145,4 @@ function getDayDate(daysAgo = 0) {
     const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
 }
-
-
 
